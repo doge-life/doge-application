48a05e40f75862c7cf28c761819cadd7a4284e47
Marc Vertido <mvertido@pillartechnology.com>
2017-02-23 17:25:33 -0500

    Hooked up service to controller

--- doge-webapp/src/main/java/main/doge/controller/FrontPageController.java
+++ doge-webapp/src/main/java/main/doge/controller/FrontPageController.java
@@ -1,13 +1,23 @@
 package main.doge.controller;
 
+import main.doge.service.TodoService;
+import main.doge.service.domain.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @RestController
 public class FrontPageController {
+
+    @Autowired
+    private TodoService todoService;
+
     @RequestMapping("/")
     public ModelAndView getFrontPage() {
-        return new ModelAndView("front");
+        List<Todo> todos = todoService.getTodos();
+        return new ModelAndView("front", "todos", todos);
     }
 }
---
+++ doge-webapp/src/test/java/main/doge/controller/FrontPageControllerTest.java
@@ -0,0 +1,44 @@
+package main.doge.controller;
+
+import main.doge.service.TodoService;
+import main.doge.service.domain.Todo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrontPageControllerTest {
+
+    @InjectMocks
+    private FrontPageController subject;
+
+    @Mock
+    private TodoService todoService;
+
+    @Test
+    public void getFrontPage_GetsDataFromService() {
+        List<Todo> expectedTodos = Arrays.asList(
+            new Todo(11, "Good-bye", false),
+            new Todo(12, "Chalon", true)
+        );
+        when(todoService.getTodos()).thenReturn(expectedTodos);
+
+        ModelAndView modelAndView = subject.getFrontPage();
+
+        assertThat(modelAndView.getViewName(), is("front"));
+        assertThat(modelAndView.getModel().get("todos"), is(expectedTodos));
+    }
+
+}
