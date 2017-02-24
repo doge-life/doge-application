9c481209f5ecf5037829b02be07439327ecc931c
Marc Vertido <mvertido@pillartechnology.com>
2017-02-24 10:43:51 -0500

    Service now does a GET on uri provided in properties file

--- doge-webapp/src/test/java/main/doge/service/TodoServiceTest.java
+++ doge-webapp/src/test/java/main/doge/service/TodoServiceTest.java
@@ -4,8 +4,6 @@ import main.doge.service.domain.Todo;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
@@ -13,25 +11,24 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TodoServiceTest {
 
-    @InjectMocks
-    private TodoService subject;
-
-    @Mock
-    private RestTemplate template;
-
     @Test
     public void getAllTodos() {
+        RestTemplate template = mock(RestTemplate.class);
+        String expectedUri = "this-is-a-uri:4567";
+        TodoService subject = new TodoService(template, expectedUri);
+
         Todo[] expectedTodos = new Todo[] {
             new Todo(1, "Hello", false),
             new Todo(2, "World", true)
         };
-        when(template.getForObject("/todos", Todo[].class)).thenReturn(expectedTodos);
+        when(template.getForObject(expectedUri + "/todos", Todo[].class)).thenReturn(expectedTodos);
 
         List<Todo> actualTodos = subject.getTodos();
 
