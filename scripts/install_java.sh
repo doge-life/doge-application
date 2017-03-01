e1d8d09d7163b4cbbe6f3d023a9cffa115a1e569
Zack Brown <zbrown@pillartechnology.com>
2017-03-01 13:45:45 -0500

    apt through the proxy

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 pushd .
 
+printf "Acquire::http::Proxy \"http://$PROXY_UNAME:$PROXY_PASSWORD@L02PIPROXY01.corp.local:80\";" > /etc/apt/apt.conf
+
 apt-get update
-apt-get install libc6-i386
+apt-get install -y libc6-i386
 
 if [[ ! -f '/tmp/jdk/jdk-8u121-linux-i586.tar.gz' ]]; then
     cd /tmp/jdk
-    apt-get install curl
+    apt-get install -y curl
     curl -L -O -H "Cookie: oraclelicense=accept-securebackup-cookie" -k "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-i586.tar.gz"
 fi
 
