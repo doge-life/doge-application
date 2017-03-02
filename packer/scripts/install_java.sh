359bb56b7c6adad1da55f458c5cce87e4117e58d
d-- <d--@users.noreply.github.com>
2017-03-02 09:49:42 -0500

    move packer stuff into its own directory

--- packer/scripts/install_java.sh
+++ packer/scripts/install_java.sh
@@ -7,6 +7,12 @@ if [[ ! -z $PROXY_HOST ]]; then
     echo "Acquire::http::Proxy \"http://$PROXY_UNAME:$PROXY_PASSWORD@$PROXY_HOST:$PROXY_PORT\";" \
         > /etc/apt/apt.conf.d/00AscenaProxy
 else
+    printf 'PROXY_HOST does not seem to have been set...\n'
+    printf 'If a proxy is needed, please set these environment variables:\n'
+    printf '    ASCENA_PROXY_HOST\n'
+    printf '    ASCENA_PROXY_PORT\n'
+    printf '    ASCENA_PROXY_UNAME\n'
+    printf '    ASCENA_PROXY_PASSWORD\n'
     printf 'Continuing without proxy...\n'
 fi
 
