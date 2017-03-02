d7131d6efa9c099cb7a3f8fe23958bb33fefc3e9
d-- <d--@users.noreply.github.com>
2017-03-02 09:15:59 -0500

    adding proxy lines to java install script, adding "pre-downloaded" folder

---
+++ downloads/README.md
@@ -0,0 +1,6 @@
+This is an area to place provisioning dependencies that you may not necessarily
+want to download each time you build the image.
+
+Examples:
+
+    jdk-8u121-linux-i586.tar.gz
--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -1,22 +1,29 @@
-#!/bin/bash
+#!/bin/bash -eu
+
 pushd .
 
 if [[ ! -z $PROXY_HOST ]]; then
-		printf "Acquire::http::Proxy \"http://$PROXY_UNAME:$PROXY_PASSWORD@L02PIPROXY01.corp.local:80\";" > /etc/apt/apt.conf.d/00AscenaProxy
+    export http_proxy="http://$PROXY_UNAME:$PROXY_PASSWORD@$PROXY_HOST:$PROXY_PORT"
+    echo "Acquire::http::Proxy \"http://$PROXY_UNAME:$PROXY_PASSWORD@$PROXY_HOST:$PROXY_PORT\";" \
+        > /etc/apt/apt.conf.d/00AscenaProxy
+else
+    printf 'Continuing without proxy...\n'
 fi
 
 apt-get update
 apt-get install -y libc6-i386
 
-if [[ ! -f '/tmp/jdk/jdk-8u121-linux-i586.tar.gz' ]]; then
-    cd /tmp/jdk
+if [[ ! -f '/tmp/downloads/jdk-8u121-linux-i586.tar.gz' ]]; then
+    mkdir -p /tmp/downloads
+    cd /tmp/downloads
     apt-get install -y curl
-    curl -L -O -H "Cookie: oraclelicense=accept-securebackup-cookie" -k "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-i586.tar.gz"
+    printf 'Downloading Java... (This might take a while)\n'
+    curl -s -L -O -H "Cookie: oraclelicense=accept-securebackup-cookie" -k \
+        "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-i586.tar.gz"
 fi
 
 cd /usr/local/lib/
-tar xvzf '/tmp/jdk/jdk-8u121-linux-i586.tar.gz'
-
+tar xvzf '/tmp/downloads/jdk-8u121-linux-i586.tar.gz'
 ln -s $PWD/jdk1.8.0_121 /usr/local/lib/java
 
 printf "\
