d618e462a65c40b1da4c3c72e87684f0ef7448f6
d-- <d--@users.noreply.github.com>
2017-03-03 16:50:16 -0500

    adding ubuntu user to docker configuration, removing 'as_root' hack

--- packer/scripts/serverspec.sh
+++ packer/scripts/serverspec.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-function as_root() {
-    if [[ $EUID != 0 ]]; then
-        sudo -i "$@"
-    else
-        "$@"
-    fi
-}
-
 source $HOME/.bash_profile
-as_root gem install bundler --no-ri --no-rdoc
+sudo gem install bundler --no-ri --no-rdoc
 cd /tmp/tests
 bundle install
 bundle exec rake spec
