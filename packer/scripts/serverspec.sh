75140b03885cda55f0dd115285b8a2c164e8446b
d-- <d--@users.noreply.github.com>
2017-03-03 10:20:56 -0500

    work around root / non-root differences in environments

--- packer/scripts/serverspec.sh
+++ packer/scripts/serverspec.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+function as_root() {
+    if [[ $EUID != 0 ]]; then
+        sudo -i "$@"
+    else
+        "$@"
+    fi
+}
+
 source $HOME/.bash_profile
-gem install bundler --no-ri --no-rdoc
+as_root gem install bundler --no-ri --no-rdoc
 cd /tmp/tests
 bundle install
 bundle exec rake spec
