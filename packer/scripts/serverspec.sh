343f31b7da29bc693cec711fe35a6006abd10d56
Remington Davenport <remington@remingtons-mbp.limitedtoo.com>
2017-03-08 13:40:03 -0500

    DOGE-58: Maisam/Remy - adding doge-webapp into amazon and docker images

--- packer/scripts/serverspec.sh
+++ packer/scripts/serverspec.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-source $HOME/.bash_profile
 sudo gem install bundler --no-ri --no-rdoc
 cd /tmp/tests
 bundle install
 bundle exec rake spec
-
--- packer/tests/spec/packages_spec.rb
+++ packer/tests/spec/packages_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe command('java -version') do
-    its(:stderr){ should include 'openjdk version "1.8' }
+  its(:stderr) { should include 'openjdk version "1.8' }
+end
+
+describe command('ps -ef | grep doge-webapp.jar') do
+  its(:stdout) { should include '/var/doge-webapp/doge-webapp.jar' }
 end
