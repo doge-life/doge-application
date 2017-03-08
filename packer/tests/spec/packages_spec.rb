4fea8acabb22479e3cfa438558e502ace07743b8
Remington Davenport <remington@remingtons-mbp.limitedtoo.com>
2017-03-08 15:35:42 -0500

    DOGE-58: Maisam/Remy - better tests. we learned a lot about serverspec

--- packer/tests/spec/packages_spec.rb
+++ packer/tests/spec/packages_spec.rb
@@ -4,6 +4,16 @@ describe command('java -version') do
   its(:stderr) { should include 'openjdk version "1.8' }
 end
 
-describe command('ps -ef | grep doge-webapp.jar') do
-  its(:stdout) { should include '/var/doge-webapp/doge-webapp.jar' }
+describe file('/var/doge-webapp/doge-webapp.jar') do
+  it { should exist }
+  it { should be_executable }
+end
+
+describe file('/etc/init.d/doge-webapp') do
+  it { should be_symlink }
+  it { should be_linked_to '/var/doge-webapp/doge-webapp.jar' }
+end
+
+describe service('doge-webapp') do
+  it { should be_enabled }
 end
