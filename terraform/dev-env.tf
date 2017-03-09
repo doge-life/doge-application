591fe07001a3e14cfc487bdae1331d15063f3e55
Zack Brown <zbrown@pillartechnology.com>
2017-03-09 12:00:25 -0500

    dev-env terraform configuration

---
+++ terraform/dev-env.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "doge-application-dev-env" {
+  ami           = "ami-1f389809"
+  instance_type = "t2.micro"
+	vpc_security_group_ids = ["${aws_security_group.doge-application-dev.id}"]
+	tags {
+    Name = "Doge Application - Development Environment"
+  }
+}
+
+resource "aws_security_group" "doge-application-dev" {
+  name = "doge-application-dev"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "TCP"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "TCP"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+}
+
+data "terraform_remote_state" "doge-application-dev-env-terraform-state" {
+    backend = "s3"
+    config {
+        bucket = "doge-application"
+        key = "terraform/dev-env-terraform.tfstate"
+        region = "us-east-1"
+    }
+}
